a0f0394981e745227977aa9507b9d69a1b8f66d0
Xinzhe Chai <xinzhe_chai@brown.edu>
2020-08-26 20:51:23 -0500

    added icon animation

--- js/hover.js
+++ js/hover.js
@@ -1,35 +1,39 @@
 window.onload = init;
 
 function init() {
-    $("#timeline-p1").on('mouseenter',function () {
+    $("#timeline-p1").on('mouseenter', function () {
         $("#timeline-description-1").addClass("timeline-opacity");
     });
 
-    $("#timeline-p1").on('mouseleave',function () {
+    $("#timeline-p1").on('mouseleave', function () {
         $("#timeline-description-1").removeClass("timeline-opacity");
     });
 
-    $("#timeline-p2").on('mouseenter',function () {
+    $("#timeline-p2").on('mouseenter', function () {
         $("#timeline-description-2").addClass("timeline-opacity");
+        $("#timeline-icon-2").removeClass("animate__fadeOutDown");
+        $("#timeline-icon-2").addClass("animate__fadeInUp");
     });
 
-    $("#timeline-p2").on('mouseleave',function () {
+    $("#timeline-p2").on('mouseleave', function () {
         $("#timeline-description-2").removeClass("timeline-opacity");
+        $("#timeline-icon-2").removeClass("animate__fadeInUp");
+        $("#timeline-icon-2").addClass("animate__fadeOutDown");
     });
 
-    $("#timeline-p3").on('mouseenter',function () {
+    $("#timeline-p3").on('mouseenter', function () {
         $("#timeline-description-3").addClass("timeline-opacity");
     });
 
-    $("#timeline-p3").on('mouseleave',function () {
+    $("#timeline-p3").on('mouseleave', function () {
         $("#timeline-description-3").removeClass("timeline-opacity");
     });
 
-    $("#timeline-p4").on('mouseenter',function () {
+    $("#timeline-p4").on('mouseenter', function () {
         $("#timeline-description-4").addClass("timeline-opacity");
     });
 
-    $("#timeline-p4").on('mouseleave',function () {
+    $("#timeline-p4").on('mouseleave', function () {
         $("#timeline-description-4").removeClass("timeline-opacity");
     });
 }
