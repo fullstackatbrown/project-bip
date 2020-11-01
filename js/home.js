300db88651eabb64d0b6251dbc7fe1f7449d8aab
Xinzhe Chai <xinzhe_chai@brown.edu>
2020-11-01 01:51:32 -0400

    experiment with loader

--- js/home.js
+++ js/home.js
@@ -77,5 +77,25 @@ function init() {
                 setTimeout(function(){ $("#timeline-textbox-4").addClass("animate__fadeInUp"); }, 525);
             }
         });
-    });
+	});
+	const preloader = document.querySelector('.preloader');
+		const fadeEffect = setInterval(() => {
+		// if we don't set opacity 1 in CSS, then   //it will be equaled to "", that's why we   // check it
+		if (!preloader.style.opacity) {
+			preloader.style.opacity = 1;
+		}
+		if (preloader.style.opacity > 0) {
+			preloader.style.opacity -= 0.5;
+		} else {
+			clearInterval(fadeEffect);
+		}
+		
+		}, 100);
+		const deleteLoader = setInterval(() => {
+			preloader.style.display = "none";
+		}, 350);
+		window.addEventListener('load', fadeEffect);
+		
+		window.addEventListener('load', deleteLoader);
+		
 }
